d88d7a2aef171eb19f324ac15c61842db44a2e59
lucidrains <lucidrains@gmail.com>
2024-10-08 09:17:14 -0700

    add the learned linear interpolation followed by norm for the residual

--- nGPT_pytorch/nGPT.py
+++ nGPT_pytorch/nGPT.py
@@ -64,11 +64,11 @@ class Attention(Module):
     ):
         super().__init__()
         dim_inner = dim_head * heads
-        self.query_weights = NormLinear(dim, dim_inner)
-        self.key_weights = NormLinear(dim, dim_inner)
-        self.value_weights = NormLinear(dim, dim_inner)
+        self.query_weights = NormLinear(dim, dim_inner, norm_dim = 0)
+        self.key_weights = NormLinear(dim, dim_inner, norm_dim = 0)
+        self.value_weights = NormLinear(dim, dim_inner, norm_dim = 0)
 
-        self.out_weights = NormLinear(dim_inner, dim, norm_dim = -1)
+        self.out_weights = NormLinear(dim_inner, dim)
 
     def forward(
         self,
@@ -85,12 +85,14 @@ class FeedForward(Module):
     ):
         super().__init__()
         dim_inner = int(dim * expand_factor * 2 / 3)
-        self.proj_in_weights = NormLinear(dim, dim_inner)
-        self.gate_weights = NormLinear(dim, dim_inner)
-        self.proj_out_weights = NormLinear(dim_inner, dim)
+        self.proj_in = NormLinear(dim, dim_inner, norm_dim = 0)
+        self.gate = NormLinear(dim, dim_inner, norm_dim = 0)
+        self.proj_out = NormLinear(dim_inner, dim)
 
     def forward(self, x):
-        return x
+        x, gate = self.proj_in(x), self.gate(x)
+        x = F.silu(gate) * x
+        return self.proj_out(x)
 
 # classes
 
@@ -104,18 +106,27 @@ class nGPT(Module):
         dim_head = 64,
         heads = 8,
         ff_expand_factor = 4.,
-        ce_ignore_index = -1
+        ce_ignore_index = -1,
+        residual_lerp_scale_init = None
     ):
         super().__init__()
+
+        residual_lerp_scale_init = default(residual_lerp_scale_init, 1. / depth)
 
         self.token_embed = NormLinear(dim, num_tokens)
 
         self.layers = ModuleList([])
+        self.residual_lerp_scales = nn.ParameterList([])
 
         for _ in range(depth):
             self.layers.append(ModuleList([
                 Attention(dim, dim_head = dim_head, heads = heads),
                 FeedForward(dim, expand_factor = ff_expand_factor),
+            ]))
+
+            self.residual_lerp_scales.append(nn.ParameterList([
+                nn.Parameter(torch.ones(dim) * residual_lerp_scale_init),
+                nn.Parameter(torch.ones(dim) * residual_lerp_scale_init),
             ]))
 
         self.token_unembed = NormLinear(dim, num_tokens, norm_dim = 0)
@@ -131,6 +142,14 @@ class nGPT(Module):
             ids, labels = ids[:, :-1], ids[:, 1:]
 
         tokens = self.token_embed.weight[ids]
+
+        for (attn, ff), (attn_alpha, ff_alpha) in zip(self.layers, self.residual_lerp_scales):
+
+            attn_out = attn(tokens)
+            tokens = l2norm(tokens.lerp(attn_out, attn_alpha))
+
+            ff_out = ff(tokens)
+            tokens = l2norm(tokens.lerp(ff_out, ff_alpha))
 
         logits = self.token_unembed(tokens)
 
