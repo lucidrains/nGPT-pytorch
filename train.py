422ae64d7688e0d8c6dac87f336a08f67e9dbec9
Phil Wang <lucidrains@gmail.com>
2024-10-10 11:22:11 -0700

    remove grad clip

--- train.py
+++ train.py
@@ -142,8 +142,6 @@ for i in tqdm.tqdm(range(NUM_BATCHES), mininterval = 10.0, desc = "training"):
 
     print(f"training loss: {loss.item():.3f}")
 
-    torch.nn.utils.clip_grad_norm_(model.parameters(), 0.5)
-
     optim.step()
     optim.zero_grad()
 
