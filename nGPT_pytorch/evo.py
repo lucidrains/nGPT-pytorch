b2acfa02684c2fa9e668dc8995c1977e52b757bd
lucidrains <lucidrains@gmail.com>
2025-03-13 08:33:09 -0700

    cross over the feedforward scales as well

--- nGPT_pytorch/evo.py
+++ nGPT_pytorch/evo.py
@@ -24,6 +24,14 @@ def cross_over(
     parent2_gate = parent2.to_gate.weight
     child_gate = child.to_gate.weight
 
+    parent1_h_scale = parent1.hidden_scale.scale
+    parent2_h_scale = parent2.hidden_scale.scale
+    child_h_scale = child.hidden_scale.scale
+
+    parent1_g_scale = parent1.gate_scale.scale
+    parent2_g_scale = parent2.gate_scale.scale
+    child_g_scale = child.gate_scale.scale
+
     parent1_w2 = parent1.to_out.weight
     parent2_w2 = parent2.to_out.weight
     child_w2 = child.to_out.weight
@@ -38,5 +46,8 @@ def cross_over(
     child_w1.copy_(cat((parent1_w1[parent1_indices], parent2_w1[parent2_indices]), dim = 0))
     child_gate.copy_(cat((parent1_gate[parent1_indices], parent2_gate[parent2_indices]), dim = 0))
     child_w2.copy_(cat((parent1_w2[:, parent1_indices], parent2_w2[:, parent2_indices]), dim = 1))
+
+    child_h_scale.copy_(cat((parent1_h_scale[parent1_indices], parent2_h_scale[parent2_indices])))
+    child_g_scale.copy_(cat((parent1_g_scale[parent1_indices], parent2_g_scale[parent2_indices])))
 
     return child
